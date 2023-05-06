8d2d840ba3407065a9f4941b8973f8a33987a761
Ozan Erk Adiguzel <ozanerk98@gmail.com>
2023-05-06 21:43:53 +0300

    v2

---
+++ src/main/java/Class01.java
@@ -0,0 +1,9 @@
+public class Class01 {
+    public static void main(String[] args) {
+        int x=1;
+        int y=2;
+        int toplam=x+y;
+        System.out.println(toplam);
+        System.out.printf("toplam", toplam);
+    }
+}
